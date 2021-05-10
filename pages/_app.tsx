0a275be7608a02987e38869e8fda80ee437c3d28
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-05-10 15:58:08 +0400

    SEO optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,15 @@ const App: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossOrigin="anonymous" />
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossOrigin="anonymous"></script>
+        <title>Kritibytes</title>
+        <meta name="description" content="We are creating tech stuff for helping community!"></meta>
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://kritibytes.tk/" />
+        <meta charSet="UTF-8" />
+        <meta property="og:title" content="Kritibytes" />
+        <meta property="og:url" content="https://kritibytes.tk/" />
+        <meta property="og:image" content="https://kritibytes.tk/banner.png" />
+        <meta property="og:description" content="We are creating tech stuff for helping community!" />
       </Head>
       <Component {...pageProps} />
     </JssProvider>
