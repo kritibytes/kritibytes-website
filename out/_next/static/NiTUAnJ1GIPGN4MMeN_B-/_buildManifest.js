e75aaf929d3551709cc632ffc3c9904e55407f0f
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-05-10 13:09:45 +0400

    Deploy gh-pages

---
+++ out/_next/static/NiTUAnJ1GIPGN4MMeN_B-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2d1183535c8028aa2b91.js"],"/_error":["static/chunks/pages/_error-01658cb27973cbff3c71.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
